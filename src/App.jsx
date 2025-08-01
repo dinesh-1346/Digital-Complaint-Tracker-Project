3796491dac637b2de0d4602054d796115d9f639f
dinesh-1346 <andlurudineshreddy@gmail.com>
2025-08-01 16:07:22 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+// frontend/src/App.jsx
+function App() {
+  return (
+    <div className="text-2xl text-center text-blue-600 mt-10">
+      Digital Complaint Tracker
+    </div>
+  );
+}
+
+export default App;
